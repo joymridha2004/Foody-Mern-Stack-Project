eee1384c99a2a6a277dc216c57f4b577f34430f1
JoY <joymridha2004@gmail.com>
2023-11-22 17:01:42 +0530

    fix some bugs

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+// const mongoURI = "mongodb+srv://joymridha2004:HBbkXgnniihVI0ha@cluster0.uq8pfou.mongodb.net/foody?retryWrites=true&w=majority"
+const mongoURI = "mongodb://joymridha2004:HBbkXgnniihVI0ha@ac-odcxiw5-shard-00-00.uq8pfou.mongodb.net:27017,ac-odcxiw5-shard-00-01.uq8pfou.mongodb.net:27017,ac-odcxiw5-shard-00-02.uq8pfou.mongodb.net:27017/?ssl=true&replicaSet=atlas-a5j1up-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+const mongoDB = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('Connected to MongoDB');
+
+    const fetchedData = await mongoose.connection.db.collection('food_items').find().toArray();
+    // console.log('Fetched data:', fetchedData);
+
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
+
+module.exports = mongoDB;
---
+++ src/components/Card.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div>
+      <div>
+        <div
+          className="card mt-3"
+          style={{ width: "18rem", maxHeight: "360px" }}
+        >
+          <img src="https://images.unsplash.com/photo-1571091718767-18b5b1457add?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJ1cmdlcnxlbnwwfHwwfHx8MA%3D%3D" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">this is some important text.</p>
+            <div className="container w-100">
+              <select className="m-2 h-100 bg-success rounded">
+                {Array.from(Array(6), (e, i) => {
+                  return (
+                    <option key={i + 1} value={i + 1}>
+                      {i + 1}
+                    </option>
+                  );
+                })}
+              </select>
+              <select className="m-2 h-100 bg-success rounded">
+                <option value="half">Half</option>
+                <option value="full">Full</option>
+              </select>
+              <div className="d-inline h-100 fs-5">Total price</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
